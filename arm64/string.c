82896a0f47b2645fd3632db720b8fcba78ad5f5d
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2015-06-16 15:44:25 -0400

    More code cleanup

--- arm64/string.c
+++ arm64/string.c
@@ -1,3 +1,22 @@
+/* 
+ ****************************************************************************
+ * (C) 2003 - Rolf Neugebauer - Intel Research Cambridge
+ ****************************************************************************
+ *
+ *        File: string.c
+ *      Author: Rolf Neugebauer (neugebar@dcs.gla.ac.uk)
+ *     Changes: 
+ *              
+ *        Date: Aug 2003
+ * 
+ * Description: Library function for string and memory manipulation
+ *              Origin unknown
+ *
+ ****************************************************************************
+ * $Id: c-insert.c,v 1.7 2002/11/08 16:04:34 rn Exp $
+ ****************************************************************************
+ */
+
 #include <types.h>
 
 void * memset(void * s,int c,size_t count)
