2983a1d763ea8b5ef164e6d1908ccf1ac694c902
Robbie VanVossen <robert.vanvossen@dornerworks.com>
2015-06-16 15:47:24 -0400

    Added a main function as an example for where to start the baremetal application

---
+++ arm64/main.c
@@ -0,0 +1,7 @@
+#include <main.h>
+
+int main(void)
+{
+	// TBD: Application goes here
+	while(1);
+}
--- arm64/setup.c
+++ arm64/setup.c
@@ -1,14 +1,7 @@
 #include <page.h>
 #include <types.h>
 #include <console.h>
-
-/*
- * Shared page for communicating with the hypervisor.
- * Events flags go here, for example.
- */
-// shared_info_t *HYPERVISOR_shared_info;
-
-extern char shared_info_page[PAGE_SIZE];
+#include <main.h>
 
 uint64_t physical_address_offset = 0;
 
@@ -17,26 +10,12 @@ uint64_t physical_address_offset = 0;
  */
 void arch_init(void *dtb_pointer, uint64_t physical_offset)
 {
-
+	// Get the physical offset for any translations
 	physical_address_offset = physical_offset;
-    // TBD: Init Code goes here
 
 	char buf[1024] = "XZD_Bare: Hello World\n";
-
 	print(buf);
 
-
-    // Start Kernel Here
-    // start_kernel();
-
-}
-
-void
-arch_fini(void)
-{
-}
-
-void
-arch_do_exit(void)
-{
+    // Start Bare Metal Application
+    main();
 }
