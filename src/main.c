301bbe95555b6bc811d1e26e9510636e7e0ff1cd
JarvisRoach <jarvis.roach@dornerworks.com>
2016-07-22 13:45:28 -0400

    Updates to load and run a payload application, that must be linked to run at 0x40400000 and require no more than 4MB, that is passed UART1 a page of shared memory at 0x7ffff000.

--- src/main.c
+++ src/main.c
@@ -1,7 +1,21 @@
 #include <main.h>
+#define START_ADDR 0x40400000
+#define SIZE  (1*1024*1024)/sizeof(unsigned long)
+extern void* _payload_start;
 
 int main(void)
 {
+    void (*fptr)(void) = (void*)START_ADDR;            
+    int i;
+    unsigned long* src_ptr = (void*)&_payload_start;
+    unsigned long* dest_ptr = (void*)START_ADDR;
+    
+    for(i = 0; i < SIZE; i++)
+       dest_ptr[i] = src_ptr[i];
+       
+
+    fptr();
+
 	// TBD: Application goes here
 	while(1);
 }
--- src/setup.c
+++ src/setup.c
@@ -13,7 +13,7 @@ void arch_init(void *dtb_pointer, uint64_t physical_offset)
 	// Get the physical offset for any translations
 	physical_address_offset = physical_offset;
 
-	char buf[1024] = "XZD_Bare: Hello World\n";
+	char buf[1024] = "XZD_Bare: App, 2016.1.x\n";
 	print(buf);
 
     // Start Bare Metal Application
