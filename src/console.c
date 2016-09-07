1b70b283342b1b8be0f2ac913bf6a2f0a30df437
JarvisRoach <jarvis.roach@dornerworks.com>
2016-09-07 12:20:08 -0400

    Added additional print_xxx functions.
    Added print_int, print_hex, print_u16, print_u32, and print_u64 to
    make it a little easier to use the Xen console for debugging the
    BMC. Also added copyrights and removed incorrect copyright/license
    from console.c

--- src/console.c
+++ src/console.c
@@ -1,45 +1,96 @@
-/* 
- ****************************************************************************
- * (C) 2006 - Grzegorz Milos - Cambridge University
- ****************************************************************************
- *
- *        File: console.h
- *      Author: Grzegorz Milos
- *     Changes: 
- *              
- *        Date: Mar 2006
- * 
- * Environment: Xen Minimal OS
- * Description: Console interface.
- *
- * Handles console I/O. Defines printk.
- *
- ****************************************************************************
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to
- * deal in the Software without restriction, including without limitation the
- * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
- * sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
- * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
- * DEALINGS IN THE SOFTWARE.
- */
+/*
+Copyright DornerWorks 2016
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the
+following conditions are met:
+1.	 Redistributions of source code must retain the above copyright notice, this list of conditions and the 
+following disclaimer.
+
+THIS SOFTWARE IS PROVIDED BY DORNERWORKS FOR USE ON THE CONTRACTED PROJECT, AND ANY EXPRESS OR IMPLIED WARRANTY 
+IS LIMITED TO THIS USE. FOR ALL OTHER USES THIS SOFTWARE IS PROVIDED ''AS IS'' AND ANY EXPRESS OR IMPLIED
+WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL DORNERWORKS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
  
 #include <types.h>
-#include <page.h>
 #include <console.h>
 #include <string.h>
-#include <xen/xen.h>
 #include <hypercall.h>
+
+void print_digit(u8 val)
+{
+	char buf[2] = {0};
+	if(val < 10)
+	{
+		buf[0] = val+'0';
+		print(buf);
+	}
+}
+
+void print_int(int val)
+{
+	int div = 1000000000;
+	int v;
+
+	if(val < 0)
+	{
+		print("-");
+		val = -val;
+	}
+
+	while(val)
+	{
+		v = val/div;
+		if(v)
+			print_digit(v);
+		val = val % div;
+		div /= 10;
+	}
+}
+
+void print_hex(u8 val)
+{
+	int nib;
+	char buf[3] = {0,0,0};
+
+		nib = (val>>4)&0xf;
+		if(nib < 10)
+			buf[0] = '0'+nib;
+		else
+			buf[0] += 'A'+nib-10;
+
+		nib = val&0xf;
+		if(nib < 10)
+			buf[1] = '0'+nib;
+		else
+			buf[1] += 'A'+nib-10;
+		print(buf);
+}
+
+void print_u16(u16 val)
+{
+	int i;
+	for(i = 1; i >= 0; i--)
+		print_hex((val>>(8*i))&0xff);
+}
+
+void print_u32(u32 val)
+{
+	int i;
+	for(i = 3; i >= 0; i--)
+		print_hex((val>>(8*i))&0xff);
+}
+
+void print_u64(u64 val)
+{
+	int i;
+	for(i = 7; i >= 0; i--)
+		print_hex((val>>(8*i))&0xff);
+}
 
 void print(char *buf)
 {
